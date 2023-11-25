af60928cbc2b175a53e8973b398f55da4db22baf
Skobelev Denis <skobelevdv01@gmail.com>
2023-11-26 01:19:26 +0300

    add menu date and ramku kalendarya

--- Verstka_Denis/ViewController.swift
+++ Verstka_Denis/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var menuBottom1: UIButton!
+    
+    @IBOutlet weak var menuBottom2: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setPopupButtom1()
+        setPopupButtom2()
     }
-
+    
+    func setPopupButtom1(){
+        let optionClosure = {(action : UIAction) in print (action.title)}
+        menuBottom1.menu = UIMenu(children : [
+            UIAction(title:"option1", state : .on , handler: optionClosure),
+            UIAction(title:"option2",  handler: optionClosure),
+            UIAction(title:"option3",  handler: optionClosure)
+        ])
+        menuBottom1.showsMenuAsPrimaryAction = true
+        menuBottom1.changesSelectionAsPrimaryAction = true
+    }
+    
+    func setPopupButtom2(){
+        let optionClosure = {(action : UIAction) in print (action.title)}
+        menuBottom2.menu = UIMenu(children : [
+            UIAction(title:"option1", state : .on , handler: optionClosure),
+            UIAction(title:"option2",  handler: optionClosure),
+            UIAction(title:"option3",  handler: optionClosure)
+        ])
+        menuBottom2.showsMenuAsPrimaryAction = true
+        menuBottom2.changesSelectionAsPrimaryAction = true
+    }
 
 }
 
